b1b6a44274d8ec872c18d83545ec4e1e593db6cc
Thomas Craessaerts <thomas.craessaerts@elia.be>
2024-06-06 15:29:37 +0200

    shell script for testing

---
+++ src/simplify_deployment/create_target.sh
@@ -0,0 +1,4 @@
+python src/simplify_deployment/create_target.py \
+    --path-to-minute-data=/home/thomas/repos/simplify_deployment/data/minute_data_simplify_study.parquet \
+    --path-to-save-target=/home/thomas/repos/simplify_deployment/data/target.parquet \
+    --number-of-weeks-to-use=16
