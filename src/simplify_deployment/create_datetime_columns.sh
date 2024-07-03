da37fdf77852b9c0d810e67b0a3c8607db0a0911
Thomas Craessaerts <thomas.craessaerts@elia.be>
2024-07-03 15:23:44 +0200

    Remove old stuff

--- src/simplify_deployment/create_datetime_columns.sh
+++
@@ -1,4 +0,0 @@
-python /home/thomas/repos/simplify_deployment/src/simplify_deployment/create_datetime_columns.py \
-    --path-to-x=/home/thomas/repos/simplify_deployment/data/minute_data_filtered_transformed.parquet \
-    --kind=ordinal \
-    --path-output=/home/thomas/repos/simplify_deployment/data/X_datetime.parquet
--- src/simplify_deployment/merge_minute_filter_transform.sh
+++
@@ -1,5 +0,0 @@
-python /home/thomas/repos/simplify_deployment/src/simplify_deployment/merge.py \
-    --paths=/home/thomas/repos/simplify_deployment/data/minute_data_merged.parquet \
-    --paths=/home/thomas/repos/simplify_deployment/data/minute_data_transformed.parquet \
-    --paths=/home/thomas/repos/simplify_deployment/data/minute_data_filtered.parquet \
-    --path-output=/home/thomas/repos/simplify_deployment/data/minute_data_filtered_transformed.parquet
