3e22b0b88a440af3be0d70ee68798557ea26d0f3
Thomas Craessaerts <thomas.craessaerts@elia.be>
2024-07-10 11:15:18 +0200

    Add more extra organisms

--- src/simplify_deployment/predict_all.sh
+++ src/simplify_deployment/predict_all.sh
@@ -5,7 +5,8 @@ python src/simplify_deployment/predict_all.py \
     --path-best-genome="/home/thomas/repos/simplify_deployment/src/simplify_deployment/genomes/lag_25_best_genome.yaml" \
     --path-to-save-predictions="/home/thomas/repos/simplify_deployment/data/predictions.parquet" \
     --extra-organisms="/home/thomas/repos/simplify_deployment/src/simplify_deployment/genomes/lag_25_simplify_1_0.yaml" \
-    --extra-organisms="/home/thomas/repos/simplify_deployment/src/simplify_deployment/genomes/lag_25_gen_2.yaml"
+    --extra-organisms="/home/thomas/repos/simplify_deployment/src/simplify_deployment/genomes/lag_25_gen_2.yaml" \
+    --extra-organisms="/home/thomas/repos/simplify_deployment/src/simplify_deployment/genomes/lag_25_best_genome.yaml"
 
 
 
